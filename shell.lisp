cc14ea6566c0dfe321aa3f342e4c8b421eff0235
Mihai Cristian Tanase <mihaicristian.tanase@gmail.com>
2022-05-22 17:25:02 +0300

    Use a better name for output parameter

--- shell.lisp
+++ shell.lisp
@@ -6,16 +6,16 @@
 
 ;; TODO(mihai): handle this case: (sb-ext:run-program "/bin/bash" '("-c" "while true; do date; sleep 1; done") :output "/tmp/a.txt")
 
-(defun run-general (has-output input &rest cmd-parts)
+(defun run-general (output input &rest cmd-parts)
   (let* ((parts (mapcar #'convert-to-string cmd-parts))
          (cmd (uiop/launch-program:escape-sh-command parts)))
     (format t "~&shell$ ~A" cmd)
-    (let* ((out (if (eq has-output t) (make-string-output-stream) has-output))
+    (let* ((out (if (eq output t) (make-string-output-stream) output))
            (proc (sb-ext:run-program "/bin/bash" (list "-c" cmd)
                                      :input input
                                      :output out
                                      :if-output-exists :supersede
-                                     :wait (not (eq has-output :stream))))
+                                     :wait (not (eq output :stream))))
            (lines (if (is-stream out)
                       (uiop/utility:split-string (get-output-stream-string out)
                                                  :separator '(#\Newline)))))
@@ -23,8 +23,8 @@
               proc))))
 
 ;; OBS(mihai): backwards compatibility
-(defun run (has-output &rest cmd-parts)
-  (apply #'run-out has-output cmd-parts))
+(defun run (output &rest cmd-parts)
+  (apply #'run-out output cmd-parts))
 
 ;; TODO(mihai): pass the parameter here
 (defun run-out (&rest cmd-parts)
@@ -33,5 +33,5 @@
 (defun run-in (input &rest cmd-parts)
   (apply #'run-general nil input cmd-parts))
 
-(defun run-in-out (has-output input &rest cmd-parts)
-  (apply #'run-general has-output input cmd-parts))
+(defun run-in-out (output input &rest cmd-parts)
+  (apply #'run-general output input cmd-parts))
